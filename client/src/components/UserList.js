398e8898dcce7d847bbb8d8eac34fdd99d39bbe2
kuldeep singh <kuldeeprathore74@gmail.com>
2022-02-23 02:18:06 +0530

    removed extra braces

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -39,19 +39,17 @@ export const UserList = () => {
         />
       </div>
       <ul className="mt-2 flex flex-col gap-4 py-2 px-2 text-center font-semibold">
-        {!filteredUsers.length > 0 ? (
+        {!filteredUsers?.length > 0 ? (
           <p>No User Found</p>
         ) : (
-          filteredUsers.map((user) => {
-            return (
-              <li
-                key={user._id}
-                className="rounded-md bg-white px-2 hover:bg-blue-400"
-              >
-                <Link to={`/profile/${user._id}`}>@{user.username}</Link>
-              </li>
-            )
-          })
+          filteredUsers.map((user) => (
+            <li
+              key={user?._id}
+              className="rounded-md bg-white px-2 hover:bg-blue-400"
+            >
+              <Link to={`/profile/${user?._id}`}>@{user?.username}</Link>
+            </li>
+          ))
         )}
       </ul>
     </div>
